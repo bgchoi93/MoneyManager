9cfb0624058659a1b8149855d493ed47a0545ee8
Brian Choi <bgchoi93@gmail.com>
2016-09-26 20:29:23 -0400

    Implementation
    - implemented date selector
    - refactor of service
    - added test mode
    - implemented distribution (not visualizer)

--- controller/analysis/core/service.py
+++ controller/analysis/core/service.py
@@ -3,6 +3,7 @@ sys.path.append('~/Documents/Project/MoneyManager')
 import model.data.dbconnection.connector as dbconn
 import pandas.io.sql as pdsql
 import model.data.data_import.csv_reader as csvreader
+import controller.analysis.core.analysis as analysis
 
 if __name__ == "__main__":
     cnxn = dbconn.InitializeConnection("localhost", 3306, "moneymanager", "")
@@ -13,17 +14,24 @@ if __name__ == "__main__":
         print("1. Import Data\n2. Analyze consumption\n3. Quit\n4. Test")
         user_input = input()
         if (user_input == "3"):
+            dbconn.CloseConnection(cnxn, cursor)
             repeat = False
         else:
             if(user_input == "1"):
                 print("Selected: Import Data")
                 csvreader.reader(cnxn, cursor)
             elif(user_input == "2"):
-                print("Analyze")
+                print("Selected: Analyze consumption")
+                analysis.analysisSelector(cnxn, cursor);
             elif(user_input == "4"):
-                sql = input("Enter query: ")
-                query_result = pdsql.read_sql(sql, cnxn)
-                print(query_result)
+                test_mode = True
+                while(test_mode):
+                    sql = input("Enter query: ")
+                    if (sql == "quit"):
+                        test_mode = False
+                    else:
+                        query_result = pdsql.read_sql(sql, cnxn)
+                        print(query_result)
             else:
                 print("Wrong Input!")
 
--- model/data/dbconnection/connector.py
+++ model/data/dbconnection/connector.py
@@ -1,8 +1,12 @@
-import pymysql;
+import pymysql
 
 def InitializeConnection (host_name, port_number, user_name, password) :
-    conn = pymysql.connect(host = host_name, port = port_number, user= user_name, passwd = password, db='moneymanagerdb');
-    return conn;
+    conn = pymysql.connect(host = host_name, port = port_number, user= user_name, passwd = password, db='moneymanagerdb')
+    return conn
 
 def ExecuteQuery () :
-    return null;
+    return null
+
+def CloseConnection (conn, cursor):
+    cursor.close()
+    conn.close()
